1367e31a1841acde3ba9695f258e8bc4e9a4e429
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-13 00:04:59 -0500

    Multiple file update
    Fixed alien.py super() mistake. Imported alien method from alien.py to main.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -20,9 +21,19 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # Set the background color
         self.bg_color = (230, 230, 230) # default color would be black, this utilizes the RGB color values. 230-230-230 is light gray
+
+    def _create_fleet(self):
+        """Create a fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -83,6 +94,7 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
