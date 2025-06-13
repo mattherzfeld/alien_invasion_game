177beda42025437560c6fc227887977395d8825c
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-12 23:57:54 -0500

    Update alien.py
    Storing the alien's starting position

--- alien.py
+++ alien.py
@@ -14,5 +14,10 @@ class Alien(Sprite):
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
+        # Start each alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
         
+        # Store the alien's exact horizontal position
+        self.x = float(self.rect.x)
         
