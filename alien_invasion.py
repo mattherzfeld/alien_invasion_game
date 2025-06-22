4ffa4d51226a4c2b00f89f1f09663fe59efdfe46
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-22 17:46:07 -0500

    Updating post-collision
    Show player the collision. Then re-center ship and decrement life count.

--- alien_invasion.py
+++ alien_invasion.py
@@ -134,7 +134,7 @@ class AlienInvasion:
 
         # Look for alien-ship collisions. "spritecollideany" takes two arguents - a sprite and a group.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print("Ship hit!")
+            self._ship_hit()
 
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
@@ -148,6 +148,21 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        # Decrement ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaning bullets and aliens.
+        self.bullets.empty()
+        self.aliens.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+        # Pause after getting hit to show player what has occurred.
+        sleep(0.5)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
