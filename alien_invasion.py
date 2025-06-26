f227849833940eaf615c6dd7057fd9acb7048343
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-25 23:27:57 -0500

    Modifying for leveling up
    Speed increase once player eliminates all aliens (new level).

--- alien_invasion.py
+++ alien_invasion.py
@@ -114,7 +114,8 @@ class AlienInvasion:
         """Start a new game when the play clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active: # This fixes that portion of the screen the play button was in (when game active, clicks won't work there).
-            # Reset the game statistics.
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.game_active = True
 
@@ -157,6 +158,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
         
 
     def _update_aliens(self):
