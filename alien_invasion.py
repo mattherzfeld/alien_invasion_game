9d2e9c23921ffd1b25e8c80349aec9701494145f
mattherzfeld <matthew.herzfeld@gmail.com>
2025-06-25 23:53:53 -0500

    Scoring
    Aliens now set to be 50 points. Some small fixes for bugs.

--- alien_invasion.py
+++ alien_invasion.py
@@ -120,6 +120,7 @@ class AlienInvasion:
             # Reset the game settings.
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -156,6 +157,11 @@ class AlienInvasion:
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+        
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
